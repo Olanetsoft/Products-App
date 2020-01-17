377c4c57287b1ad014cb8c7cf4026077638dad45
olanetsoft <heedris2olubisi@gmail.com>
2020-01-17 17:22:21 +0100

    UPDATED CLOCK.JS WITH COMMENTS ON HOW EACH FUNCTION WORKS

--- src/component/clock.js
+++ src/component/clock.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 class Clock extends Component {
     constructor(props){
         super(props);
+
+        //This declared the state of time at the very beginning
         this.state ={
             time: new Date().toLocaleString()
         }
         
     }
 
+    //This happens when the component mount and the setInterval function get called with a call back function updateClock()
     componentDidMount() {
         this.intervalID = setInterval(() =>
             this.updateClock(),
@@ -16,10 +19,12 @@ class Clock extends Component {
         );
     }
 
+    //This section clears setInterval by calling intervalID so as to optimise memory
     componentWillUnmount(){
         clearInterval(this.intervalID)
     }
 
+    //This function set the state of the time to a new time
     updateClock(){
         this.setState({
             time: new Date().toLocaleString()
