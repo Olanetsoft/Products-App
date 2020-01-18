838f18098959fc219605aafd7f278474216e71bd
olanetsoft <heedris2olubisi@gmail.com>
2020-01-18 15:37:50 +0100

    CHANGE COLOR BUTTON COMPONENT COMMENTED IN DETAILS

--- src/component/changeColorButton.js
+++ src/component/changeColorButton.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class ChangeColorButton extends Component {
     constructor(props){
         super(props);
+        //This section below is use to bind the handleClick function so as not to loose reference
         this.handleClick = this.handleClick.bind(this)
     }
+
+    //This function is created to get its properties from the parent by props
     handleClick(){
         this.props.onClick();
     }
