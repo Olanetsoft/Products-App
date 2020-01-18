5ec45103e00f4746f9728d70559cc377ed760faf
olanetsoft <heedris2olubisi@gmail.com>
2020-01-18 15:34:35 +0100

    COLOR BLOCK COMPONENT COMMENTED CORRRECTLY

--- src/component/colorBlock.js
+++ src/component/colorBlock.js
@@ -5,12 +5,16 @@ import ChangeColorButton from './changeColorButton';
 class ColorBlock extends Component {
     constructor(props){
         super(props);
+
+        //This state is set at the beginning
         this.state = { 
             backgroundColor: "red"
         };
+        //This section below is use to bind the changeColor function so as not to loose reference
         this.changeColor = this.changeColor.bind(this);
     }
 
+    //This function is created to set the new state and also check the current color so as enable toggling between the two colors
     changeColor(){
         this.setState((prevState) =>{
             let newColor = prevState.backgroundColor === "red" ? "green" : "red"
